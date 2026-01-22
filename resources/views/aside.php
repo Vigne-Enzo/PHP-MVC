5dd9d1fd77f537024560df74c12833f24c812a7a
vignee <vigne.enzo73@gmail.com>
2026-01-22 15:24:30 +0100

    Déplacement formulaire

--- resources/views/aside.php
+++ resources/views/aside.php
@@ -1,16 +1,8 @@
 <aside>
 
-      <h2>Ajout d'un article</h2>
-
-      <p>Les données du formulaire seront envoyées vers le serveur en méthode HTTP "post" et seront disponibles côté serveur dans la variable PHP $_POST</p>
-
-      <form action="" method="post">
-          <label for="article_title">Titre : </label>
-          <input type="text" name="article_title">
-          <label for="article_content">Contenu : </label>
-          <textarea name="article_content"></textarea>
-          <input type="submit" value="Ajouter" name="ajout_article">
-      </form>
+    <?php   
+      require 'articles/form.php';
+    ?>
 
 
       <h2>Lien avec des paramètres</h2>
