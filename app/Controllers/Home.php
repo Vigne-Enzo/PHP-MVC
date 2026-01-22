114f7909ecb38ab7f96fe2fdd32ffae2e379faa7
vignee <vigne.enzo73@gmail.com>
2026-01-22 15:37:42 +0100

    MVC - Activité 1 - Fin

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,11 +4,22 @@ require_once 'app/Models/Article.php';
 
 class Home
 {
-  function index()
-  {
-    $article_model = new Article();
+    function index()
+    {
+        $article_model = new Article();
 
-    $articles = $article_model->list();
-    require "resources/views/template.php";
-  }
+        $articles = $article_model->list();
+        require "resources/views/template.php";
+    }
+
+    function add()
+    {
+        if (isset($_POST['ajout_article'])) {
+            $article_model = new Article();
+            $article_model->add($_POST);
+        }
+
+        $articles = $article_model->list();
+        require "resources/views/template.php";
+    }
 }
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
 require 'app/bootstrap.php';
-require_once 'app/Controllers/Home.php';
-$controller = new Home();
-$controller->index();
+require 'routes.php';
 ?>
