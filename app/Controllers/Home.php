7936272b13bb96fe6463633a858c5c42a1599eac
vignee <vigne.enzo73@gmail.com>
2026-01-26 15:14:27 +0100

    activité 2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -10,13 +10,18 @@ class Home
         $article_model = new Article();
         $articles = $article_model->list();
 
-        $view_aside = new View([], ['form' => 'articles/form'], 'aside', false);
+        $view_aside = new View([], [
+            'form' => 'articles/form',
+            'blocks' => [
+                'blocks/link_session_variables',
+                'blocks/link_get_parameters'
+            ]
+        ], 'aside', false);
 
         new View(['articles' => $articles], ['content' => 'articles/list', 'aside' => $view_aside]);
-        // affichier blocks/link_session_variables.php et  blocks/link_get_parameters.php dans l'aside
-        
-        
-    }   
+
+
+    }
 
     function add()
     {
@@ -24,8 +29,30 @@ class Home
             $article_model = new Article();
             $article_model->add($_POST);
         }
+        $this->index();
+    }
 
-        $articles = $article_model->list();
-        require "resources/views/template.php";
+    function delete($id)
+    {
+        $article_model = new Article();
+        $article_model->delete($id);
+        $this->index();
+    }
+
+    function edit($id)
+    {
+
+        if (isset($_POST['edition_article'])) {
+            $article_model = new Article();
+            $article_model->update($_POST);
+            return $this->index();
+        }
+
+        $article_model = new Article();
+        $article_model = $article_model->get($id);
+
+        $view_aside = new View([], ['form' => 'articles/form', 'get' => 'blocks/link_get_parameters', 'session' => 'blocks/link_session_variables'], 'aside', false);
+
+        new View(['article' => $article_model], ['content' => 'articles/edit', 'aside' => $view_aside]);
     }
 }
